2b936ad9eccef6d91cf8550a8f20763effad84e1
Kyle David <kzd0038@tigermail.auburn.edu>
2022-09-29 10:12:44 -0500

    Update primary_analyses.r

--- primary_analyses.r
+++ primary_analyses.r
@@ -9,23 +9,25 @@ library(effsize)
 
 #create directory to hold results
 dir.create("Amphibia_results")
+
+#read environmental data table (for example: HERE)
 geo <- read.csv("Amphibia_geodata.txt", sep='\t')
 
-# load ploidy inferences
-ploidy <- read_excel("canonical_polyploids.xlsx", sheet = "Amphibia", col_names = c("species", "ploidy", "source", "comments"), na = "NA")
+#load ploidy inferences (Table S3 in manuscript)
+ploidy <- read_excel("Table_S3.xlsx", sheet = "Amphibia", col_names = c("species", "ploidy", "source", "comments"), na = "NA")
 
-#use more conservative polyploid inferences
-ploidy[!is.na(ploidy$comments),]$ploidy <- NA
+#alternative ploidy assignment using a more strict definition of polyploidy (see manuscript for details)
+#ploidy[!is.na(ploidy$comments),]$ploidy <- NA
 
+#remove unnecessary columns
 ploidy <- ploidy[1:2]
-ploidy <- ploidy[ploidy$species %in% geo$species,]
 
-# merge
+#merge environmental data table with ploidy assignments
 df <- merge(geo, ploidy, all = TRUE)
 df[!(df$species %in% ploidy$species),]$ploidy <- 0
 rownames(df) <- unlist(df$species)
 
-# remove diploids without chromosome count data
+#alternative ploidy assignment using only species with confirmed chromosome counts (see manuscript for details)
 #count <- read.csv('Amphibia_count.txt', sep='\t', col.names = c('species', 'chr_count'), header=FALSE)
 #diploids <- df[df$ploidy == 0,]$species
 #suspect_diploids <- diploids[!(diploids %in% count$species)]
@@ -33,12 +35,9 @@ rownames(df) <- unlist(df$species)
 
 # read tree
 tree <- read.tree("Amphibia_tree.nh")
-tree <- multi2di(tree)
 tree$tip.label <- gsub("_", " ", tree$tip.label)
-tree <- keep.tip(tree, df$species)
-tree$tip.label[is.na(tree$tip.label)] <- "trim"
-tree <- drop.tip(tree, c("trim"))
 
+#for every environmental variable, calculate Cohen's D and perform phylogenetic ANOVA 
 var_names <- colnames(df[c(2:15)])
 
 cohens_d <- c()
@@ -58,19 +57,20 @@ anova_p <- c(anova_p, summary(test)$coefficients[2,4])
     
     }
 
+#grab summary statistics and write output file
 results_table <- data.frame(var = var_names, cohens.d = cohens_d, anova.p = anova_p)
-write.table(results_table, 'Amphibia_polyver_results/Amphibia_table.txt', quote = FALSE, row.names = FALSE, sep = '\t')
+write.table(results_table, 'Amphibia_results/Amphibia_table.txt', quote = FALSE, row.names = FALSE, sep = '\t')
 
+#get every combination of variables found significant following Bonferroni correcion
 sig_vars <- results_table[results_table$anova.p < 0.05/length(var_names),]$var
 res <- Map(combn, list(sig_vars), seq_along(sig_vars), simplify = FALSE)
 res <- unlist(res, recursive = FALSE)
-
-datalist = list()
 
 #center data for MANOVA
 df <- df %>%
   mutate_at(var_names, scale, scale = FALSE)
 
+#perform MANOVA on every combination of significant variables and report summary AIC scores
 for (vars in res) {
     manova.df <- na.omit(df)
     dat <- data.frame(manova.df[unlist(vars)])
@@ -78,11 +78,13 @@ for (vars in res) {
     group <- factor(manova.df$ploidy)
     names(group) <- unlist(manova.df$species)
     phy.manova <- aov.phylo(dat~group, tree, nsim = 1)
-    write(paste(c(paste(unlist(vars), collapse='+'), extractAIC(phy.manova)[2]), collapse='\t'),"Amphibia_polyver_results/Amphibia_models.txt", append=TRUE)}
-    
+    write(paste(c(paste(unlist(vars), collapse='+'), extractAIC(phy.manova)[2]), collapse='\t'),"Amphibia_results/Amphibia_models.txt", append=TRUE)}
+
+#assign ploidy randomly by sampling without replacement from empirical inferences
 random_polyploids <- sample(df$species, length(df[which(df$ploidy==1),]$species), FALSE)
 random_NA <- sample(df$species, length(df[which(is.na(df$ploidy)),]$species))
 
+#run same analysis as above on null dataset
 df$dummy_ploidy <- 0
 df[which(df$species %in% random_polyploids),]$dummy_ploidy <- 1
 df[which(df$species %in% random_NA),]$dummy_ploidy <- NA
@@ -105,5 +107,5 @@ anova_p <- c(anova_p, summary(test)$coefficients[2,4])
     }
 
 results_table <- data.frame(var = var_names, cohens.d = cohens_d, anova.p = anova_p)
-write.table(results_table, 'Amphibia_polyver_results/Amphibia_null_table.txt', quote = FALSE, row.names = FALSE, sep = '\t')
+write.table(results_table, 'Amphibia_results/Amphibia_null_table.txt', quote = FALSE, row.names = FALSE, sep = '\t')
 
